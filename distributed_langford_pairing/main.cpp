bb8d39d0ecdd800f79751a4e4d79aba56dc2e17b
cgerloff <cgerloff@fuchs.de>
2014-12-22 10:16:52 +0100

    Initial Commit

---
+++ distributed_langford_pairing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  distributed_langford_pairing
+//
+//  Created by Gerloff, Christian on 22.12.14.
+//  Copyright (c) 2014 Gerloff, Christian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
